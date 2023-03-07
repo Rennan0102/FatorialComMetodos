53c10ff757fa1a721068eeb7655375cc11ffa709
Rennan <rennan.fernandess@gmail.com>
2023-03-07 14:42:06 -0300

    Full Application

---
+++ src/pacotes/Fatorial.java
@@ -0,0 +1,29 @@
+package pacotes;
+
+public class Fatorial {
+    private int numero = 0;
+    private int fatorial = 1;
+    private String formula = "";
+    
+    public void setValor(int num){   // Calcula o fatorial
+        numero = num;
+        int fat = 1;
+        String str = "";
+        
+        for (int cont = num; cont > 1; cont--){
+            fat *= cont;
+            str += cont + " X ";
+        }
+        str += "1 = ";
+        fatorial = fat;
+        formula = str;
+    }
+    
+    public int getFatorial(){
+        return fatorial;
+    }
+    
+    public String getFormula(){
+        return formula;
+    }
+}
